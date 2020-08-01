a6c3592f0ce1ce77a8b705d4c1664c1d20bcc94c
Rojar Smith <rojarsmith@gmail.com>
2020-08-01 23:29:58 +0800

    Import login page. Add the assist of development.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 
 //Page
 import LandingPage from "views/LandingPage/LandingPage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
 
 class App extends Component {
   constructor(props) {
@@ -19,6 +20,7 @@ class App extends Component {
     return (
       <div>
         <Switch>
+          <Route path="/login-page" component={LoginPage} />
           <Route exact path="/" component={LandingPage} />
         </Switch>
       </div>
