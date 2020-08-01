f5c6378d78785ca4e086bbe2343eb77d6206c453
Rojar Smith <rojarsmith@gmail.com>
2020-08-01 12:24:58 +0800

    Import react-alert.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Alert
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
 //Redux
 import { Provider } from 'react-redux'
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import { account } from './redux/reducers'
+
+//Alert
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: "30px",
+  transition: transitions.SCALE,
+};
 
 //Redux
 const logger = createLogger();
@@ -25,7 +37,9 @@ const render = () => {
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <AlertProvider template={AlertTemplate} {...options}>
+          <App />
+        </AlertProvider>
       </Provider>
     </React.StrictMode>,
     document.getElementById('root')
