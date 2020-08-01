647586108059c3c6af14cb9b63e206e40c931517
Rojar Smith <rojarsmith@gmail.com>
2020-08-01 12:39:11 +0800

    Add the Route schema in App.js.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Alert
+import { withAlert } from 'react-alert';
+
+//Router
+import { Route, Switch, withRouter } from 'react-router-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(withAlert()(App));
