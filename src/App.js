a1f153fc80b1b6b124fd4b15e7fb020fcfc3f6ad
Rojar Smith <rojarsmith@gmail.com>
2020-08-02 08:09:52 +0800

    Import ecommerce page.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 //Page
 import AboutUsPage from "views/AboutUsPage/AboutUsPage.js";
 import ContactUsPage from "views/ContactUsPage/ContactUsPage.js";
+import EcommercePage from "views/EcommercePage/EcommercePage.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
 import SignupPage from "views/SignupPage/SignupPage.js";
@@ -25,6 +26,7 @@ class App extends Component {
         <Switch>
           <Route path="/about-us" component={AboutUsPage} />
           <Route path="/contact-us" component={ContactUsPage} />
+          <Route path="/ecommerce-page" component={EcommercePage} />
           <Route path="/login-page" component={LoginPage} />
           <Route path="/signup-page" component={SignupPage} />
           <Route path="/" component={LandingPage} />
