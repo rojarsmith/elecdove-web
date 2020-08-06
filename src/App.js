b3a2395e4994ebbb174a73f6f574aa2ac81cf530
Rojar Smith <rojarsmith@gmail.com>
2020-08-06 08:33:39 +0800

    Add example for check development or release environment.

--- src/App.js
+++ src/App.js
@@ -53,7 +53,9 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    console.log("App/componentDidMount");
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+      console.log("App/componentDidMount");
+    }
     this.loadCurrentlyLoggedInUser();
   }
 
