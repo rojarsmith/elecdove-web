8b38c8740687b13e510c9220a0b377398d00efa0
Rojar Smith <rojarsmith@gmail.com>
2020-10-08 21:42:01 +0800

    Fix CD bug with npm build.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -38,7 +38,7 @@ module.exports = {
       path: '~/service/web/elecdove-web',
       'pre-deploy-local': '',
       'pre-setup': '',
-      'post-deploy': 'npm install -g serve && npm run build',
+      'post-deploy': 'npm install -g serve && npm run build && pm2 start ecosystem.config.js --env production',
       env_production: {
         NODE_ENV: 'production'
       }
